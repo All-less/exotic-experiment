50432ee0b69cb2243d0a2437b9b1e651792ae632
lmzqwer2 <lmzqwerty@163.com>
2016-04-27 12:21:16 +0800

    Rewrite the client.py

--- server/client.py
+++ server/client.py
@@ -14,58 +14,8 @@ def prompt():
     sys.stdout.write('<You> ')
     sys.stdout.flush()
 
-class FileDownload(threading.Thread):
-    def __init__(self, host, webport, link, filetype, callback):
-        threading.Thread.__init__(self)
-        self.link = 'http://%s:%d%s%s' % (host, webport, link, filetype)
-        self.callback = callback
-
-    def run(self):
-        h = httplib2.Http(timeout = 1)
-        (resp_headers, content) = h.request(self.link, "GET")
-        self.callback(resp_headers, content)
-
-def downloadend(resp_headers, content):
-    #with open("./test", "wb") as f:
-    #    f.write(content)
-    s.send(json.dumps(dict(
-        type = Type.status,
-        status = "bit_file_programmed",
-        size = len(content)
-    )) + separator)
-
-if len(sys.argv) < 3:
-    print 'Usage : python ', __file__,' hostname port'
-    sys.exit()
-
-host = sys.argv[1]
-port = int(sys.argv[2])
-device_id = 'test'
-auth_key = '8f5aff8cb424d2cf39b206619f8d1461'
-separator = '\n'
-
-if len(sys.argv) > 4:
-    device_id = sys.argv[3]
-    auth_key = sys.argv[4]
-
-if len(sys.argv) > 5:
-    separator = sys.argv[5]
-
-filelink = None
-webport = None
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# connect to remote host
-try :
-    s.connect((host, port))
-    s.setblocking(0)
-except :
-    print 'Unable to connect'
-    sys.exit()
-
-
-print '''
+def printHelp():
+    print '''
 Connected to remote host. Start sending messages
 1 Input
   'auth'
@@ -79,131 +29,219 @@ Connected to remote host. Start sending messages
 4 Input
   'exit'
   to exit this program.
-'''
-prompt()
+    '''
 
-exit = False
+class ServerSocket:
+    class FileDownload(threading.Thread):
+        def __init__(self, host, webport, link, filetype, callback):
+            threading.Thread.__init__(self)
+            self.link = 'http://%s:%d%s%s' % (host, webport, link, filetype)
+            self.callback = callback
 
-while not exit:
-    rlist = [sys.stdin, s]
+        def run(self):
+            h = httplib2.Http(timeout = 10)
+            (resp_headers, content) = h.request(self.link, "GET")
+            self.callback(resp_headers, content)
 
-    # Get the list sockets which are readable
-    read_list, write_list, error_list = select.select(rlist , [], [])
+    def __init__(self, host, port, device_id, auth_key, separator):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try :
+            s.connect((host, port))
+            s.setblocking(0)
+            self.s = s
+        except :
+            print 'Unable to connect'
+            sys.exit()
+        self.filelink = None
+        self.webport = None
+        self.host = host
+        self.port = port
+        self.device_id = device_id
+        self.auth_key = auth_key
+        self.separator = separator
+        self.buffer = ''
 
+    def downloadend(self, resp_headers, content):
+        #with open("./test", "wb") as f:
+        #    f.write(content)
+        self.send_Status("bit_file_programmed", size = len(content))
 
-    for sock in read_list:
-        #incoming message from remote server
-        if sock == s:
-            data = sock.recv(4096)
-            if not data :
-                print '\nDisconnected from chat server'
-                sys.exit()
-            else :
-                #print datan
-                remain = ''
-                datalist = data.split(separator)[:-1]
-                for data in datalist:
-                    data = remain + data
-                    print '\n Server: ',
+    def download(self, filetype):
+        filethread = self.FileDownload(self.host, self.webport, self.filelink, filetype, self.downloadend)
+        filethread.start()
+
+    def send(self, message):
+        self.s.send(message + self.separator)
+
+    def send_JSON(self, **kw):
+        self.send(json.dumps(kw))
+
+    def send_Type(self, msgtype, **kw):
+        kw['type'] = msgtype
+        self.send_JSON(**kw)
+
+    def send_Action(self, message, **kw):
+        kw['action'] = message
+        self.send_Type(Type.action, **kw)
+
+    def send_authorize(self, device_id=None, auth_key=None):
+        if device_id == None:
+            device_id = self.device_id
+        if auth_key == None:
+            auth_key = self.auth_key
+        self.send_Action(Action.authorize, device_id = device_id, auth_key = auth_key)
+
+    def send_Status(self, message, **kw):
+        kw['status'] = message
+        self.send_Type(Type.status, **kw)
+
+    def send_keyPress(self, key_code):
+        self.send_Status('key_pressed', key_code = key_code)
+
+    def send_switchOn(self, id):
+        self.send_Status('switch_on', id = id)
+
+    def send_switchOff(self, id):
+        self.send_Status('switch_off', id = id)
+
+    def send_buttonPress(self, id):
+        self.send_Status('button_pressed', id = id)
+
+    def send_buttonRelease(self, id):
+        self.send_Status('button_released', id = id)
+
+    def handleOperation(self, data):
+        operation = data.get("operation", None)
+        if operation is not None:
+            status = OperationReverse.get(str(operation), None)
+            if status is not None:
+                keyname = 'key_code'
+                value = data.get("key_code", None)
+                if value is None:
+                    value = data.get('id', -1)
+                    keyname = 'id'
+                d = dict()
+                d[keyname] = value
+                self.send_Status(status, **d)
+
+    def handleStatus(self, data):
+        status = data.get("status", None)
+        if status == Status.authorized:
+            self.filelink = data.get("filelink")
+            self.webport = data.get("webport")
+        elif status == Status.file_upload:
+            self.download(data['file']['type'])
+
+    def handleInfo(self, data):
+        info = data.get('info', None)
+        if info == Info.user_changed:
+            print 'User change to %s' % data.get('user', None)
+
+    def dataRecv(self, data):
+        l = data.split(self.separator)
+        remain = l[-1]
+        for message in l[:-1]:
+            self.buffer += message
+            self.on_read(self.buffer)
+            self.buffer = ''
+        self.buffer = l[-1]
+
+    def on_read(self, message):
+        data = json.loads(message)
+
+        print '\nserver', json.dumps(data)
+        messageType = data.get('type', None)
+
+        if messageType == Type.action:
+            pass
+        elif messageType == Type.status:
+            self.handleStatus(data)
+        elif messageType == Type.operation:
+            self.handleOperation(data)
+        elif messageType == Type.info:
+            self.handleInfo(data)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print 'Usage : python ', __file__,' hostname port'
+        sys.exit()
+
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+    device_id = 'test'
+    auth_key = '8f5aff8cb424d2cf39b206619f8d1461'
+    separator = '\n'
+
+    if len(sys.argv) > 4:
+        device_id = sys.argv[3]
+        auth_key = sys.argv[4]
+    if len(sys.argv) > 5:
+        separator = sys.argv[5]
+
+    printHelp()
+
+    prompt()
+
+    exit = False
+
+    server = ServerSocket(host, port, device_id, auth_key, separator)
+
+    while not exit:
+        rlist = [sys.stdin, server.s]
+
+        # Get the list sockets which are readable
+        read_list, write_list, error_list = select.select(rlist , [], [])
+
+        for sock in read_list:
+            #incoming message from remote server
+            if sock == server.s:
+                data = sock.recv(4096)
+                if not data :
+                    print '\nDisconnected from chat server'
+                    sys.exit()
+                else :
+                    server.dataRecv(data)
+                    prompt()
+
+            #user entered a message
+            else:
+                msg = sys.stdin.readline()
+                msg = msg[:-1]
+                try:
+                    funcEqual = {
+                        'auth' : server.send_authorize,
+                        'exit' : sys.exit,
+                    }
+                    funcOneParam = {
+                        'keypress' : server.send_keyPress,
+                        'switchon' : server.send_switchOn,
+                        'switchoff': server.send_switchOff,
+                        'buttonpress': server.send_buttonPress,
+                        'buttonrelease': server.send_buttonRelease,
+                    }
+                    access = False
                     try:
-                        data = json.loads(data)
-                        remain = ''
-                    except ValueError, e:
-                        remain += separator + data
-                        continue
-
-                    sys.stdout.write(json.dumps(data))
-                    messageType = data.get('type', None)
-
-                    if messageType == Type.action:
+                        for (key, func) in funcEqual.items():
+                            if msg == key:
+                                func()
+                                access = True
+                                break
+                    except Exception, e:
                         pass
-                    elif messageType == Type.status:
-                        status = data.get("status", None)
-                        if status == Status.authorized:
-                            filelink = data.get("filelink")
-                            webport = data.get("webport")
-                        elif status == Status.file_upload:
-                            filethread = FileDownload(host, webport, filelink, data['file']['type'], downloadend)
-                            filethread.start()
-                    elif messageType == Type.operation:
-                        operation = data.get("operation", None)
-                        if operation is not None:
-                            status = OperationReverse.get(str(operation), None)
-                            if status is not None:
-                                keyname = 'key_code'
-                                value = data.get("key_code", None)
-                                if value is None:
-                                    value = data.get('id', -1)
-                                    keyname = 'id'
-                                sendDict = dict(
-                                    type = Type.status,
-                                    status = status,
-                                )
-                                sendDict[keyname] = value
-                                s.send(json.dumps(sendDict) + separator)
-                    elif messageType == Type.info:
-                        print 'User change to %s' % data.get('user', None)
-
-                    print
+                    try:
+                        a, b = msg.split(' ')
+                        for (key, func) in funcOneParam.items():
+                            if a == key:
+                                b = int(b)
+                                func(b)
+                                access = True
+                                break
+                    except Exception, e:
+                        pass
+                    if not access:
+                        server.send(msg)
+                except Exception, e:
+                    print e.message
                 prompt()
 
-        #user entered a message
-        else :
-            msg = sys.stdin.readline()
-            msg = msg[:-1]
-            try:
-                if msg == 'auth':
-                    s.send(json.dumps(dict(
-                        type = Type.action,
-                        action = Action.authorize,
-                        device_id = device_id,
-                        auth_key = auth_key,
-                    )) + separator)
-                elif msg.startswith('keypress'):
-                    a, b = msg.split(' ')
-                    b = int(b)
-                    s.send(json.dumps(dict(
-                        type = Type.status,
-                        status = 'key_pressed',
-                        key_code = b
-                    )) + separator)
-                elif msg.startswith('switchon'):
-                    a, b = msg.split(' ')
-                    b = int(b)
-                    s.send(json.dumps(dict(
-                        type = Type.status,
-                        status = 'switch_on',
-                        id = b
-                    )) + separator)
-                elif msg.startswith('switchoff'):
-                    a, b = msg.split(' ')
-                    b = int(b)
-                    s.send(json.dumps(dict(
-                        type = Type.status,
-                        status = 'switch_off',
-                        id = b
-                    )) + separator)
-                elif msg.startswith('buttonpress'):
-                    a, b = msg.split(' ')
-                    b = int(b)
-                    s.send(json.dumps(dict(
-                        type = Type.status,
-                        status = 'button_pressed',
-                        id = b
-                    )) + separator)
-                elif msg.startswith('buttonrelease'):
-                    a, b = msg.split(' ')
-                    b = int(b)
-                    s.send(json.dumps(dict(
-                        type = Type.status,
-                        status = 'button_released',
-                        id = b
-                    )) + separator)
-                elif msg == 'exit':
-                    exit = True
-                else:
-                    s.send(msg + separator)
-            except Exception, e:
-                print e.message
-            prompt()
-s.close()
+    server.s.close()
