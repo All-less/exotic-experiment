8685bb9ebc07f954c8451d5716fcdaab2bc6413b
Loy <puregithub@yeah.net>
2016-09-07 22:13:11 +0800

    Remove unnecessary dependency.

--- ext/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd src
-gcc -shared -o libbcm2835.so -fPIC bcm2835.c
-cp libbcm2835.so /usr/local/lib
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-export LD_LIBRARY_PATH
-ldconfig
